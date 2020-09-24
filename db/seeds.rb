50910b8c11cc5aebd3dd313be1b63af5a00c5ee8
sparsell <hello@sparsell.com>
2020-09-24 05:16:57 -0400

    trying to de/reseed database with test data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,11 @@
 #seed data to test program
 
-charlotte = User.create(name: "Charlotte", email: "charlotte.parsell@icloud.com")
+charlotte = User.create(name: "Charlotte", email: "charlotte@charlotte.com")
 
-thomas = User.create(name: "Thomas", email: "thomas.parsell@icloud.com")
+thomas = User.create(name: "Thomas", email: "thomas@thomas.com")
 
-bobby = User.create(name: "Bobby", email: "bobby@icloud.com")
-
+dan = User.create(name: "Dan", email: "dan@dan.com")
+sue = User.create(name: "Sue", email: "sue@sue.com")
 
 device = Device.create(device_type: "ipad", limit: true, can_text: true)
 device1 = Device.create(device_type: "iphone", limit: true, can_text: true)
