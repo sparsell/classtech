08d4c35e64c50ceb103410681402b49bbf95e61b
sparsell <hello@sparsell.com>
2020-09-29 14:23:05 -0400

    cleared seeds.rb file for more accurate data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,1 @@
 #seed data to test program
-
-charlotte = User.create(name: "Charlotte", email: "charlotte@charlotte.com")
-
-thomas = User.create(name: "Thomas", email: "thomas@thomas.com")
-
-dan = User.create(name: "Dan", email: "dan@dan.com")
-sue = User.create(name: "Sue", email: "sue@sue.com")
-
-device = Device.create(device_type: "ipad", limit: true, can_text: true)
-device1 = Device.create(device_type: "iphone", limit: true, can_text: true)
-device2 = Device.create(device_type: "macbook", limit: true, can_text: true)
