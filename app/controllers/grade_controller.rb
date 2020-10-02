093f6fe8c21b368df978198b521c74ebcfe11716
sparsell <hello@sparsell.com>
2020-10-02 14:45:25 -0400

    added grade controller

--- app/controllers/grade_controller.rb
+++ app/controllers/grade_controller.rb
@@ -1,3 +1,10 @@
 class GradeController < ApplicationController
 
+    #want to show all children and devices by grade
+    
+    #table like this:
+    #grade 3
+        #child -- device -- behaviors
+
+
 end
