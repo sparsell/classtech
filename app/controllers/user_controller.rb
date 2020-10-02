787e2eabe3c0d8beaee82220c5913472a4d07ec9
sparsell <hello@sparsell.com>
2020-10-02 15:41:35 -0400

    misc edit

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,39 +1,14 @@
 class UserController < ApplicationController
 
-
-    get '/registrations/signup' do
-    #render signup form
-        erb :'/registrations/signup'
-    end 
-
-    post '/registrations' do
-    #gets the new user's info from params hash
-    #create a new user
-    #signs them in
-    #redirects them 
+    get '/users/profile' do
+        erb :'users/profile'
+    end
+    
+    get '/users/:id' do 
+        @user = User.find(params[:id])
+        erb :'users/profile'
     end
 
-    get '/sessions/login' do
-        #rendering the login form
-        erb :'/sessions/login'
-    end
-
-    post '/sessions/login' do
-        #gets user info from params hash
-        #matches agains the existing entries in db
-        #if matching, signs in, or else redirect to signup
-    end
-
-    get '/sessions/logout' do
-    #clear the session hash
-    end
-
-    get '/user/profile' do
-        erb :'user/profile'
-    end
-
-    get '/user/signup' do
-        erb :'user/signup'
-    end
+   
 
 end
