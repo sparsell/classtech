7f66be5d69864ef00d77e244c351df796b0c200f
sparsell <hello@sparsell.com>
2020-10-02 05:10:46 -0400

    signup route

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,3 +1,12 @@
 class UserController < ApplicationController
 
+
+    get '/user/profile' do
+        erb :'user/profile'
+    end
+
+    get '/user/signup' do
+        erb :'user/signup'
+    end
+
 end
