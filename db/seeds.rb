87d132d4ba477f641f91470ffcb8d65bf5ae9a95
sparsell <hello@sparsell.com>
2020-10-06 06:11:52 -0400

    started creating seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,5 @@
 #seed data to test program
+sonja = User.create(name: "Sonja")
+    charlotte = Child.create(name: "Charlotte", owner: sonja, grade: 5)
+    Device.create(name: "iPad", owner: charlotte)
+    Device.create(name: "Chromebook", owner: charlotte)
