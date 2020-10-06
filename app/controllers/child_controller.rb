99ca6982679b4e9d3eadd96fd20102f4634ba29c
sparsell <hello@sparsell.com>
2020-10-06 14:29:53 -0400

    added child edit view  and controller routes

--- app/controllers/child_controller.rb
+++ app/controllers/child_controller.rb
@@ -1,3 +1,14 @@
 class ChildController < ApplicationController
 
+    post '/children' do
+        @child = Child.create(:name => params[:name]) 
+        redirect to "/children/#{@child.id}"
+      end
+    
+      get '/children/:id/edit' do 
+        @child = Child.find(params[:id])
+        @devices = Device.all
+        erb :'/children/edit'
+      end
+
 end
