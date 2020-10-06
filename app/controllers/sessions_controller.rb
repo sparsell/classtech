9a888680817c9a1848047b9c6ca3550fcda46478
sparsell <hello@sparsell.com>
2020-10-06 06:39:38 -0400

    updating to user_name from name for login requirements

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,15 +6,15 @@ class SessionsController < ApplicationController
     end
 
     post '/sessions' do #from login form params hash
-     #gets user info from params hash
         #matches agains the existing entries in db
         #if matching, signs in, or else redirect to signup
-        @user = User.find_by(name: params["name"])
-        if @user != nil && @user.password = params["password"]
+        @user = User.find_by(name: params[:user_name])
+        if @user != nil && @user.password == params["password"]
         session[:id] = @user.id
-        redirect to '/users/:id'
+        redirect "/users/#{@user.id}"
+        
         else
-            redirect to '/sessions/login'
+            redirect '/sessions/login'
         end
     end
 
