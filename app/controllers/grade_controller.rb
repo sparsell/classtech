3dcda4c072fae20c3480d51864f8cfd5116ad9b5
sparsell <hello@sparsell.com>
2020-10-06 06:02:51 -0400

    added route to view grades/index to grade_controller

--- app/controllers/grade_controller.rb
+++ app/controllers/grade_controller.rb
@@ -2,9 +2,8 @@ class GradeController < ApplicationController
 
     #want to show all children and devices by grade
     
-    #table like this:
-    #grade 3
-        #child -- device -- behaviors
-
+    get '/grades' do
+        erb :'grades/index'
+    end
 
 end
