fc962d88dfde19f86f9762ba6b1477e4fb2c0aed
sparsell <hello@sparsell.com>
2020-10-09 14:35:19 -0400

    added grades index.erb  and route

--- app/controllers/grade_controller.rb
+++ app/controllers/grade_controller.rb
@@ -3,11 +3,12 @@ class GradeController < ApplicationController
     #want to show all children and devices by grade
     
     get '/grades' do
+        @grades = Grade.all
         erb :'grades/index'
     end
 
     post '/grades' do
-        @grade = Grade.all(grade: params)
+        @grades = Grade.all
         erb :'grades/show'
     end
 
