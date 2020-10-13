082b18d9013e033190b03f3c50f54b739cbc14fd
sparsell <hello@sparsell.com>
2020-10-13 13:04:37 -0400

    wip-views and routes for child show page after entering a new child

--- app/controllers/child_controller.rb
+++ app/controllers/child_controller.rb
@@ -10,16 +10,19 @@ class ChildController < ApplicationController
     erb :'children/new' 
   end 
 
+  post '/children' do
+    @child = Child.create(name: params[:child][:name], grade_id: params[:child][:grade_id], device_ids: params[:child][:device_ids]) 
+    if !params[:device][:device_type].empty?
+        @child.devices << Device.create(device_type: params[:device][:device_type])
+        @child.user_id = session[:user_id]
+    end
+    erb :'/children/show'
+  end
+
   get '/children/:id' do
-   @child = Child.find_by(name: params[:name])
+   @child = Child.find_by(name: params[:child][:name])
     erb :'children/show' 
   end 
-  
-  post '/children' do
-    @child = Child.create(:name => params[:name]) 
-  
-    redirect to "/children/#{@child.id}"
-  end
     
   get '/children/:id/edit' do 
     @child = Child.find(params[:id])
