32b7c9dea82f0a40e31b407a9a06713ed2c723a3
sparsell <hello@sparsell.com>
2020-10-15 16:27:53 -0400

    testing and debugging all routes and views for children - wip

--- app/controllers/child_controller.rb
+++ app/controllers/child_controller.rb
@@ -1,9 +1,6 @@
 class ChildController < ApplicationController
 
-  get '/children' do
-    erb :'children/show'
-  end
-
+   #### CREATE ####
   get '/children/new' do
     @grades = Grade.all
     @devices = Device.all
@@ -11,24 +8,45 @@ class ChildController < ApplicationController
   end 
 
   post '/children' do
-    @child = Child.create(name: params[:child][:name], grade_id: params[:child][:grade_id], device_ids: params[:child][:device_ids]) 
+    @child = Child.create(name: params[:child][:name], grade_id: params[:child][:grade_id],  device_ids: params[:child][:device_ids], user_id: current_user.id) 
+    
+    #select from checkbox or create new but need to control for duplicates!
     if !params[:device][:device_type].empty?
-        @child.devices << Device.create(device_type: params[:device][:device_type])
-        @child.user_id = session[:user_id]
+        @child.devices << Device.create(params[:device])
+        # @child.devices << Device.create(device_type: params[:device][:device_type])
     end
-    erb :'/children/show'
+    @child.save
+    
+    redirect "/children/#{@child.id}"
+  end
+
+
+  #### READ ####
+  get '/children' do
+    erb :'children/show'
+    # erb :'children/index' (but not showing all the kids to user...or could?)
   end
 
   get '/children/:id' do
-    #when a link brings you here there are no params...
-   @child = Child.find_by(name: params[:child][:name])
-    erb :'children/show' 
+    @child = Child.find_by(params[:id])
+    erb :"/children/show" 
   end 
     
+  ### UPDATE ###
+  
   get '/children/:id/edit' do 
     @child = Child.find(params[:id])
-    @devices = Device.all
-    erb :'/children/edit'
+
+    erb :'children/edit'
   end
+
+  patch '/children/:id' do
+    @child = Child.find_by_id(params[:id])
+    @child.update(params(:child))
+    
+    redirect "/children/#{@child.id}"
+  end
+
+  ### DELETE ###
 
 end
