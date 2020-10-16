c808212dfdf909be77bd2b5a34836c9094c0620a
sparsell <hello@sparsell.com>
2020-10-16 05:59:16 -0400

    reviewing functionality and necessity of devices index and show views

--- app/controllers/device_controller.rb
+++ app/controllers/device_controller.rb
@@ -1,5 +1,6 @@
 class DeviceController < ApplicationController
 
+    ### No plan to have a view of the devices in the app - do I need to delete the controller then and all the views? (best practice?)
     get '/devices' do
         @devices = Device.all 
         erb :'devices/index'
