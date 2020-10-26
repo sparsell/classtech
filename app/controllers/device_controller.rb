16b65f4ae2cb8eba861c67b4853e0f1158a1da99
sparsell <hello@sparsell.com>
2020-10-26 13:41:06 -0400

    added show and index view and controller actions for devices

--- app/controllers/device_controller.rb
+++ app/controllers/device_controller.rb
@@ -1,8 +1,19 @@
 class DeviceController < ApplicationController
 
-    ### No plan to have a view of the devices in the app - do I need to delete the controller then and all the views? (best practice?)
     get '/devices' do
         @devices = Device.all 
+        # binding.pry
         erb :'devices/index'
     end
+
+    get '/devices/:id' do
+        @device = Device.find(params[:id])
+        @children = @device.children  #array of all the kids with this device - from the join table
+        if @children.empty?
+            flash[:message] = "There aren't any children who have this type of device."
+        end
+        erb :'devices/show'
+        
+    end
+
 end
