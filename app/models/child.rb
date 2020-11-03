be73e323093ef8337168067eb6fff222be8de7c4
sparsell <hello@sparsell.com>
2020-11-03 16:09:01 -0500

    added rules and child_rules associations to child class

--- app/models/child.rb
+++ app/models/child.rb
@@ -4,5 +4,7 @@ class Child < ActiveRecord::Base
     belongs_to :grade
     has_many :child_devices
     has_many :devices, through: :child_devices
+    has_many :child_rules
+    has_many :rules, through: :child_rules
 
 end
