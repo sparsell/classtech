3639e86c82735aeab2a02e9ee5a73d49c54afb09
sparsell <hello@sparsell.com>
2020-11-03 15:49:46 -0500

    schema changes due to adding rule MVC

--- db/schema.rb
+++ db/schema.rb
@@ -11,34 +11,37 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20201026100839) do
+ActiveRecord::Schema.define(version: 20201027174115) do
 
   create_table "child_devices", force: :cascade do |t|
     t.integer "child_id"
     t.integer "device_id"
   end
 
+  create_table "child_rules", force: :cascade do |t|
+    t.integer "child_id"
+    t.integer "rule_id"
+  end
+
   create_table "children", force: :cascade do |t|
     t.string  "name"
     t.integer "user_id"
     t.integer "grade_id"
-    t.boolean "has_limits" #Stretch
-    t.boolean "can_text"   #Stretch
-    t.boolean "can_chat"   #Stretch
-    t.boolean "soc_media"  #Stretch
-    t.boolean "play_games" #Stretch
-    t.boolean "watch_yt"   #Stretch
   end
 
   create_table "devices", force: :cascade do |t|
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.string   "device_type"
-    t.boolean  "limits"   #not required but can't remove for some reason (migration hasn't workded)
+    t.boolean  "limits"
   end
 
   create_table "grades", force: :cascade do |t|
     t.integer "grade"
+  end
+
+  create_table "rules", force: :cascade do |t|
+    t.string "rule_name"
   end
 
   create_table "users", force: :cascade do |t|
